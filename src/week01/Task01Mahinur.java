adabbb221ddfc032f3ea5613b4f76e83780f0337
mahinur <mahinur301@hotmail.com>
2023-09-27 22:09:04 +0200

    odd even updated

---
+++ src/week01/Task01Mahinur.java
@@ -0,0 +1,22 @@
+package week01;
+
+public class Task01Mahinur {
+
+    public static void main(String[] args) {
+        oddOrEven(7);
+        oddOrEven(8);
+    }
+
+
+    public static void oddOrEven(int n) {
+        if (n % 2 == 0) {
+            System.out.println("It is an even number");
+        } else {
+            System.out.println(" It is an odd number. ");
+
+        }
+    }
+}
+
+
+
