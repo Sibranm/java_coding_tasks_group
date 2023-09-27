f51081bba91e5af11826b36ae7eb86c7f55ce4a8
Sibranm <sibranmuzepper@gmail.com>
2023-09-27 22:20:20 +0200

    Task01sima updated

--- src/week01/Task01sima.java
+++ src/week01/Task01sima.java
@@ -2,6 +2,14 @@ package week01;
 
 public class Task01sima {
     public static void main(String[] args) {
+        identify(20);
+    }
 
+    public static void identify(int num){
+        if(num % 2 == 0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
     }
 }
