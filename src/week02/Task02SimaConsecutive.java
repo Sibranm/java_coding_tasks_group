d689330c602669da6d631af82b84f1e4543e1213
Sibranm <sibranmuzepper@gmail.com>
2023-10-04 22:38:47 +0200

    update Task02SimaConsecutive.java

--- src/week02/Task02SimaConsecutive.java
+++ src/week02/Task02SimaConsecutive.java
@@ -1,4 +1,28 @@
 package week02;
 
 public class Task02SimaConsecutive {
+
+    public static void main(String[] args) {
+        consecutiveNums(20);
+    }
+    public static void consecutiveNums(int num){
+
+        for(int i = 1; i <= num; i++){
+
+            String output = "";
+
+            if(i % 2 == 0){
+                output += "Codility";
+            }
+            if(i % 3 == 0){
+                output += "Test";
+            }
+            if(i % 5 == 0){
+                output += "Coders";
+            }
+            System.out.println(output.isEmpty() ? i : output);
+        }
+
+
+    }
 }
