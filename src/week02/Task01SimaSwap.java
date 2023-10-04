ba6b0784a3a00bac73829a95362fd33c5d82c2a7
Sibranm <sibranmuzepper@gmail.com>
2023-10-04 22:52:46 +0200

    update Task01SimaSwap.java

--- src/week02/Task01SimaSwap.java
+++ src/week02/Task01SimaSwap.java
@@ -1,4 +1,19 @@
 package week02;
 
 public class Task01SimaSwap {
+     /*
+    Swap two variables value without using a third variable
+     */
+
+    public static void main(String[] args) {
+        swapNums(7, 19);
+    }
+    public static void swapNums(int num1, int num2) {
+
+        int sum = num1 + num2;
+        num1 = sum - num1;
+        num2 = sum - num2;
+
+        System.out.println(num1 + "," + num2);
+    }
 }
