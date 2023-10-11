4367bb09edf68f7aaf7bfc2024138e10e31fb44b
Sibranm <sibranmuzepper@gmail.com>
2023-10-11 22:56:02 +0200

    create IsPrimeNumberSima.java

--- src/week03/IsPrimeNumberSima.java
+++ src/week03/IsPrimeNumberSima.java
@@ -6,4 +6,25 @@ public class IsPrimeNumberSima {
     Write a method that can check if a number is
     prime or not
      */
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(1));
+        System.out.println(isPrime(9));
+        System.out.println(isPrime(13));
+    }
+    public static boolean isPrime(int num){
+
+        if(num == 1){
+            return false;
+        }
+
+        for (int i = 2; i <= num/2; i++) {
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+
+
+    }
 }
