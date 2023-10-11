988a2cd7a9ecc5c9643b95c01b7b27eb9dd798f4
Sibranm <sibranmuzepper@gmail.com>
2023-10-11 22:46:55 +0200

    create ReverseNegativeSima.java

--- src/week03/ReverseNegativeSima.java
+++ src/week03/ReverseNegativeSima.java
@@ -2,8 +2,37 @@ package week03;
 
 public class ReverseNegativeSima {
     /*
-    1. Numbers -- Prime Number
-    Write a method that can check if a number is
-    prime or not
+
      */
+    public static void main(String[] args) {
+        System.out.println(reverseNegative(-123));
+    }
+
+    public static int reverseNegative(int num){
+
+        if(num >= 0){
+            return num;
+        }else {
+
+            String number = num + "";
+            String[] arr = number.split("");
+            String[] newArr = new String[arr.length - 1];
+
+            for (int i = arr.length - 1, j = 0; i > 0; i--, j++) {
+                newArr[j] = arr[i];
+            }
+
+            String output = "";
+
+            for (String each : newArr) {
+                output += each;
+            }
+
+            int newNum = Integer.parseInt(output);
+
+            return -newNum;
+        }
+
+    }
+
 }
