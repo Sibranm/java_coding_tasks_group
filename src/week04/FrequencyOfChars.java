8b8f5b329eb9dca8f235354d48b6ffe1649d17b3
Sibranm <sibranmuzepper@gmail.com>
2023-10-23 21:25:12 +0200

    FrequencyOfChars.java updated

---
+++ src/week04/FrequencyOfChars.java
@@ -0,0 +1,41 @@
+package week04;
+
+public class FrequencyOfChars {
+    public static void main(String[] args) {
+        frequencyOfChars("AAABBCDD");
+        frequencyOfChars("aabcccd");
+    }
+
+    public static void frequencyOfChars(String str) {
+
+        String result = "";
+        for (
+                int i = 0; i < str.length(); i++) {
+            String let = str.charAt(i) + "";
+            int count = 0;
+            if (!result.contains(let)) {
+                result += let;
+                for (int j = i; j < str.length(); j++) {
+                    if (let.equals(str.charAt(j) + "")) {
+                        count++;
+                    }
+                }
+                result += count;
+            }
+        }
+        System.out.println(result);
+
+        /* solution with ArrayList:
+        String[] arr = str.split("");
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(arr));
+
+
+        String result = "";
+        for (String each : list) {
+            int count = Collections.frequency(list,each);
+            result += (!result.contains(each)) ? (each + count) : "";
+        }
+        System.out.println(result);*/
+    }
+
+}
